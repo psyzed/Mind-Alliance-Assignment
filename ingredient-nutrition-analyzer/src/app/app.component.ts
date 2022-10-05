5ec37fa842adb9d7340d4d0daff0344919de5fd6
Lazaros Poumpouridis <psyzed66@gmail.com>
2022-10-05 14:15:05 +0300

    Added form controls and button functionality

--- ingredient-nutrition-analyzer/src/app/app.component.ts
+++ ingredient-nutrition-analyzer/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ingredient-nutrition-analyzer';
+
+  ingredientForm: FormGroup;
+
+  ngOnInit() {
+    this.ingredientForm = new FormGroup({
+      name: new FormArray([new FormControl(null, Validators.required)]),
+    });
+  }
+
+  onAddIngredients() {
+    const ingredientInput = new FormControl(null, Validators.required);
+    (<FormArray>this.ingredientForm.get('name')).push(ingredientInput);
+  }
+
+  onDelete(index: number) {
+    (<FormArray>this.ingredientForm.get('name')).removeAt(index);
+  }
+
+  onSubmit() {
+    console.log(this.ingredientForm);
+  }
+
+  get controls() {
+    return (<FormArray>this.ingredientForm.get('name')).controls;
+  }
 }
