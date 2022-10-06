3840a004afdaeee495594856e3bf5028327a4919
Lazaros Poumpouridis <psyzed66@gmail.com>
2022-10-06 11:19:16 +0300

    Added loagind indicator and did some minor changes

--- ingredient-nutrition-analyzer/src/app/nutrition.service.ts
+++ ingredient-nutrition-analyzer/src/app/nutrition.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import * as interfaces from '../app/analyzer-result.interface';
 
 @Injectable({ providedIn: 'root' })
 export class NutritionService {
   constructor(private http: HttpClient) {}
 
-  submitIngredients(ingredient: string) {
-    console.log(ingredient);
+  submitIngredients(ingredient: string[]) {
     return this.http.post(
       'https://api.edamam.com/api/nutrition-details?app_id=' +
         environment.appId +
